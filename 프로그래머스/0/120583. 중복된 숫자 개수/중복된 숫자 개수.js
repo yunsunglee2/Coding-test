c354009142bdb90e4993c4896d4206e2a05f588a
yunsungLEE <135799803+yunsunglee2@users.noreply.github.com>
2024-10-04 10:23:37 +0900

    [level 0] Title: 중복된 숫자 개수, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120583. 중복된 숫자 개수/중복된 숫자 개수.js
+++ 프로그래머스/0/120583. 중복된 숫자 개수/중복된 숫자 개수.js
@@ -1,13 +1,10 @@
 function solution(array, n) {
     var answer = 0;
     
-    // 정수 집합 배열 array, 정수 n 
-    // array에 n이 몇개 있는지 찾아라 
-    // includes -> 요소를 매개변수로 받고 부울린 값 반환 
-    // find -> 조건을 매개변수로 받고 부울린 값 반환 
-    // filter를 통한 새로운 배열을 반환 받은 뒤 길이를 구해 반환
-    const filteredArray = array.filter((el)=> el === n)
-    answer = filteredArray.length
+    //배열을 순회하면서 el === n 이면은 answer 카운트를 더해서 반환한다. 
+    array.forEach((el)=> {
+        el === n ? answer += 1 : answer += 0
+    })
     
     return answer;
 }
