90aa15a70edf7113051967b7ba3c3d98f31e97c1
yunsungLEE <135799803+yunsunglee2@users.noreply.github.com>
2024-10-04 11:01:01 +0900

    [level 0] Title: 배열의 유사도, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.js
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.js
@@ -3,12 +3,17 @@ function solution(s1, s2) {
     
     // 두 배열을 비교하여 같은 원소를 개수 구하기 
     
-    // 이중 반복문으로 순회하면서 answer에 카운트 +1 
-    s1.forEach((el_1) => {
-        s2.forEach((el_2) => {
-            el_1 === el_2 ? answer+= 1 : answer+= 0;
-        })
-    })
+    // 1. 이중 반복문으로 순회하면서 answer에 카운트 +1 
+    // s1.forEach((el_1) => {
+    //     s2.forEach((el_2) => {
+    //         el_1 === el_2 ? answer+= 1 : answer+= 0;
+    //     })
+    // })
+    
+    // 2. filter 메서드를 통해 s1의 요소가 s2에 includes 되어 있는지 순회하고 배열의 길이로 반환
+    const filteredArr = s1.filter((el) => s2.includes(el));
+    
+    answer = filteredArr.length;
     
     return answer;
 }
