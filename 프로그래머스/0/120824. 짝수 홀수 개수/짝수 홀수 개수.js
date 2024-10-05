6e36ff47c8c8f8e3aedf6e467139aad20539e165
yunsungLEE <135799803+yunsunglee2@users.noreply.github.com>
2024-10-05 13:50:10 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.06 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -1,15 +1,14 @@
 function solution(num_list) {
-    var answer = [];
+    var answer = [0,0];
     
-    // 짝수는 2로 나누어서 나머지값이 0인 숫자, 홀수는 2로 나누어서 나머지가 1인 숫자
-    
-    // num_list를 filter를 통해 조건에 맞는 배열을 반환 받고 길이를 answer에 push 
-    const even_number_array = num_list.filter((el)=> el%2 === 0)
-    const odd_number_array = num_list.filter((el)=> el%2 === 1)
-    
-    // 개수를 담은 배열을 반환 
-    answer.push(even_number_array.length)
-    answer.push(odd_number_array.length)
+    // 배열 순회하며 (단순반복)
+    num_list.forEach((num) => {
+        if(num % 2 === 0) {
+            answer[0] += 1
+        } else {
+            answer[1] += 1
+        }
+    })
     
     return answer;
 }
