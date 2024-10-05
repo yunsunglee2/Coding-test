51ea21d934c67ac3ba5e25c39bb22e7c96ea739f
yunsungLEE <135799803+yunsunglee2@users.noreply.github.com>
2024-10-05 13:20:17 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 74.04 ms, Memory: 69.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,26 +1,26 @@
 function solution(participant, completion) {
     var answer = '';
+    // 참가 배열, 완주 배열
+    // participant = completion + 1
+    // 선수? 참여? -> 사람? -> 명단? -> 해시
     
-    //마라톤 선수, 단 한명 제외 마라톤 완주 com - parti = - 1
-    
-    // 해시 맵 생성 
+    // 객체로 명단 생성 (키: 이름, 벨류: 명수)
     const participantList = {};
     
-    // partcipant를 반복문으로 순회하며 명단 생성, 카운트 + 1
-    participant.forEach((name)=> {
+    // 참가자 배열을 순회하며 명단 객체에 키, 벨류 추가하기 (단순반복-> 반복문)
+    participant.forEach((name) => {
         participantList[name] = (participantList[name] || 0) + 1
     })
     
-    // completion를 반복문으로 순회하며 명단 검사, 명단에 이름 있으면 0, 아니면 그대로 1
-    completion.forEach((name)=> {
-        if(participantList[name]) {
-            participantList[name] = participantList[name] - 1    
-        }  
+    // 완주가 명단 순회하며 명단 객체에 이름이 있다면 +1
+    completion.forEach((com_name) => {
+        participantList[com_name] = participantList[com_name] - 1
     })
     
-    // 완주 못한 선수 카운트 1인 사람 반환
-    for (const name in participantList){
-        if(participantList[name] === 1) return name;
+    // 객체 명단을 순회하며 값이 0 보다 키 찾아 answer로 반환
+    for(const person in participantList) {
+        if(participantList[person] > 0) return person;
     }
     
+    return answer;
 }
