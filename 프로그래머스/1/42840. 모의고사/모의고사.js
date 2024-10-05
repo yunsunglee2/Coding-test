84ff2447d909cbd9b019ea4d1bd792d1a754fa4d
yunsungLEE <135799803+yunsunglee2@users.noreply.github.com>
2024-10-05 16:09:13 +0900

    [level 1] Title: 모의고사, Time: 4.38 ms, Memory: 37 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -1,31 +1,30 @@
 function solution(answers) {
     var answer = [];
     const scores = [0,0,0];
-    // 패턴 정의
+    
+    //  패턴 
     const patterns = [
         [1,2,3,4,5],
         [2,1,2,3,2,4,2,5],
         [3,3,1,1,2,2,4,4,5,5]
-    ];
-    
-    // answers를 순회하며 정답비교, 스코어 카운트 +1
-    answers.forEach((el_1,i) => {
-        patterns.forEach((el_2,j) => {
-            if(el_1 === el_2[i%el_2.length]) {
-                scores[j]+=1
+    ]
+    // 완전 탐색
+    answers.forEach((item,i) => {
+        patterns.forEach((pattern,j) => {
+            const index = i%pattern.length
+            if (pattern[index] === item) {
+                scores[j] += 1;
             }
         })
     })
     
-    console.log(scores);
-    
-    // scores를 순회하며 가장 많이 맞힌 사람 배열로 반환
-    
-    const maxScore = Math.max(...scores)
+    // 결과 도출
+    const maxScore = Math.max(...scores);
     
     scores.forEach((score, i) => {
-        score === maxScore && answer.push(i+1)
+        if(score === maxScore) {
+            answer.push(i+1)
+        }
     })
-    
     return answer;
 }
