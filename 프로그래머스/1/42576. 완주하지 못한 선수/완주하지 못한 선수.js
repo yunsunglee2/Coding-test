b9f82d06be227cfa5de32c79b5e9eaf9e04f375c
yunsungLEE <135799803+yunsunglee2@users.noreply.github.com>
2024-10-08 09:57:40 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 69.82 ms, Memory: 70.1 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,26 +1,28 @@
 function solution(participant, completion) {
     var answer = '';
-    // 참가 배열, 완주 배열
-    // participant = completion + 1
-    // 선수? 참여? -> 사람? -> 명단? -> 해시
     
-    // 객체로 명단 생성 (키: 이름, 벨류: 명수)
+    // 명단 만들어서 사람 찾기 
     const participantList = {};
     
-    // 참가자 배열을 순회하며 명단 객체에 키, 벨류 추가하기 (단순반복-> 반복문)
+    // participant 순회하면서 participantList에 키, 벨류 생성 카운트 + 1 (중복 존재시, 그대로 유지)
     participant.forEach((name) => {
         participantList[name] = (participantList[name] || 0) + 1
     })
     
-    // 완주가 명단 순회하며 명단 객체에 이름이 있다면 +1
-    completion.forEach((com_name) => {
-        participantList[com_name] = participantList[com_name] - 1
+    // completion 순회하면서 participantList에 있으면 카운트 -1
+    completion.forEach((name) => {
+        if(participantList[name]) {
+               participantList[name] = participantList[name] - 1
+        }
     })
     
-    // 객체 명단을 순회하며 값이 0 보다 키 찾아 answer로 반환
+    // participantList에서 for...in 문 순회하면서 answer 반환
     for(const person in participantList) {
-        if(participantList[person] > 0) return person;
+        if(participantList[person] > 0) {
+            answer = person
+        }
     }
+    
     
     return answer;
 }
