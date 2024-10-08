024c584b1f0cb00ab9290fdbb4783a48b6199564
yunsungLEE <135799803+yunsunglee2@users.noreply.github.com>
2024-10-08 10:04:18 +0900

    [level 1] Title: K번째수, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,21 +1,20 @@
 function solution(array, commands) {
     var answer = [];
     
-    // slice(begin, end) end 미포함, 인덱스, 길이 주의
+    // i ~ j 자르고 정렬 , k 번쨰 수 구하기 
+    
+    // slice(begin, end) 번째, 인덱스 구별 주의 
+    
+    // commands 순회하면서 slice, sort 하고 answer에 Push
     commands.forEach((command) => {
-        const i = command[0];
-        const j = command[1];
-        const k = command[2];
-    // i 번째 ~ j 번쨰 자르고 sort    
-        const sortedArray = array.slice(i-1,j).sort((a,b) => a-b);
-    // k 번째 수 구하기 (인덱스 주의)
-        answer.push(sortedArray[k-1]);
+        const i = command[0]
+        const j = command[1]
+        const k = command[2]
+        
+        const sortedArray = array.slice(i - 1,j).sort((a,b) => a - b)
+        answer.push(sortedArray[k-1])
+        
     })
-    
-    
-    
-    
-    
     
     return answer;
 }
