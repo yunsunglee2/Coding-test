0145aefc3c1159b1287d9ad5fa1bbcb47c72e09f
yunsungLEE <135799803+yunsunglee2@users.noreply.github.com>
2024-10-08 09:42:02 +0900

    [level 1] Title: 폰켓몬, Time: 0.58 ms, Memory: 34.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,21 +1,13 @@
 function solution(nums) {
     var answer = 0;
     
-    // n 마리 포켓몬, n/2 마리 가져가기 가능 
-    // 번호를 붙여 구분(같은 종류 === 같은 번호)
-    // ex. 4마리 포켓몬 3 1 2 3
-    // n / 2 마리 포켓몬 선택 방법 중, 가장 많은 종류의 포켓몬 선택 방법 
+    // N 마리 중에서 N/2 마리 픽, 최대한 다양한 종류 포켓몬 픽 
     
-    // 가져가기 허용 갯수
     const allowedCount = nums.length/2
     
-    // 배열에서 중복 제거하기
-    const deletedNums = new Set(nums)
+    const deletedCount = new Set(nums);
     
-    // 중복 제거 배열 요소의 조합 계산하기
-    const deletedNumsLength = deletedNums.size
-    
-    answer = Math.min(deletedNumsLength, allowedCount)
+    answer = Math.min(allowedCount, deletedCount.size)
     
     return answer;
 }
