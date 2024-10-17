4fa0afdbb0a3e7ba193e373e42dbeba588a11556
yunsungLEE <135799803+yunsunglee2@users.noreply.github.com>
2024-10-17 12:13:14 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.js
@@ -0,0 +1,13 @@
+function solution(n) {
+    var answer = [];
+    
+    // split으로 주어진 숫자 배열화
+    const spiltedArray = String(n).split('');
+    
+    // 배열을 for로 순회하며 뒤에 부터 answer 배열에 push
+    for(let i = spiltedArray.length-1; i >= 0; i--) {
+        answer.push(Number(spiltedArray[i]))
+    }
+    
+    return answer;
+}
