d87b8ff3acc721f6088fe03fe767e4378e89c650
yunsungLEE <135799803+yunsunglee2@users.noreply.github.com>
2024-10-19 14:11:07 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 17.11 ms, Memory: 41.9 MB -BaekjoonHub

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -1,8 +1,9 @@
 function solution(arr) {
     var answer = [];
+    const minVal = Math.min(...arr);
     
     // Math.min()을 통해 가장 작은 수 제거
-    const filteredArr = arr.filter((el) => Math.min(...arr) !== el)
+    const filteredArr = arr.filter((el) => minVal !== el)
     
     // 빈 배열 -1 push
     if(filteredArr.length === 0) {
