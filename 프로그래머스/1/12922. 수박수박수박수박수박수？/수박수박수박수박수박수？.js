a5a68f54dbd4fd2b5fb11ce7eb7c1fa736ea26c3
yunsungLEE <135799803+yunsunglee2@users.noreply.github.com>
2024-10-22 16:39:45 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 1.20 ms, Memory: 34.1 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -1,11 +1,10 @@
 function solution(n) {
     var answer = '';
     const WORD = "수박";
-    const str_length = WORD.length;
     
     // 반복문을 통해서 해결 
     for (let i  = 0; i < n; i++) {
-        answer += WORD[i%str_length]
+        answer += WORD[i%WORD.length]
     }
     
     return answer;
