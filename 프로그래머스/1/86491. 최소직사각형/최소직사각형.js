64654f91f14a0f18264ff150b175ad38574ef310
yunsungLEE <135799803+yunsunglee2@users.noreply.github.com>
2025-03-01 14:36:54 +0900

    [level 1] Title: 최소직사각형, Time: 2.71 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
@@ -1,20 +1,18 @@
 function solution(sizes) {
     var answer = 0;
-    // 찾고자하는 값 변수 선언
-    let hori = 0;
-    let verti = 0;
-    
-    // 원단길이 순회
-    sizes.forEach((size) => {
-        // 눕힙다 -> 가로, 세로 길이 변경한다 -> 기준: 긴 값 = 가로 통일
+    const result = {
+        larger: 0,
+        smaller: 0,
+    }
+    // sizes를 순회하면서 대소 비교를 진행한다. 
+    sizes.forEach(size => {
         const larger = Math.max(size[0], size[1]);
         const smaller = Math.min(size[0], size[1]);
         
-        hori = Math.max(smaller, hori);
-        verti = Math.max(larger,verti);
-    });
-    
-    answer = hori*verti ;
+        result.larger = Math.max(larger, result.larger);
+        result.smaller = Math.max(smaller, result.smaller)
+    })
+    answer = result.larger*result.smaller;
     
     return answer;
 }
