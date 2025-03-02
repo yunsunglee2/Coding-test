e84b427d309d442d8dc4018d122db9226ea40d97
yunsungLEE <135799803+yunsunglee2@users.noreply.github.com>
2025-03-02 15:21:16 +0900

    [level 1] Title: 모의고사, Time: 25.72 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -1,30 +1,20 @@
 function solution(answers) {
-    var answer = [];
-    const scores = [0,0,0];
+    const scores = [];
+    const answer = [];
+    const patterns = [[1,2,3,4,5],[2, 1, 2, 3, 2, 4, 2, 5],[3,3,1,1,2,2,4,4,5,5]];
     
-    //  패턴 
-    const patterns = [
-        [1,2,3,4,5],
-        [2,1,2,3,2,4,2,5],
-        [3,3,1,1,2,2,4,4,5,5]
-    ]
-    // 완전 탐색
-    answers.forEach((item,i) => {
-        patterns.forEach((pattern,j) => {
-            const index = i%pattern.length
-            if (pattern[index] === item) {
-                scores[j] += 1;
-            }
+    patterns.forEach((pattern) => {
+        let score = 0;
+        answers.forEach((answer,i) => {
+            answer === pattern[i%pattern.length] && score++;
         })
+        scores.push(score);
     })
-    
-    // 결과 도출
-    const maxScore = Math.max(...scores);
-    
-    scores.forEach((score, i) => {
-        if(score === maxScore) {
-            answer.push(i+1)
-        }
+    const MAX_SCORE = Math.max(...scores);
+    // 점수가 같은 경우 처리 
+    scores.forEach((score,i) => {
+        score === MAX_SCORE && answer.push(i+1)
     })
+    console.log(answer)
     return answer;
 }
