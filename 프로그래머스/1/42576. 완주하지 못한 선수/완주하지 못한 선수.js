aef65930ed5eda634f6b9eabd7de53f22479fd59
yunsungLEE <135799803+yunsunglee2@users.noreply.github.com>
2025-03-05 20:14:20 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 73.93 ms, Memory: 69.5 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,28 +1,27 @@
 function solution(participant, completion) {
     var answer = '';
+    // 이름-인원수 표 만들기
+    const participants = {};
     
-    // 명단 만들어서 사람 찾기 
-    const participantList = {};
-    
-    // participant 순회하면서 participantList에 키, 벨류 생성 카운트 + 1 (중복 존재시, 그대로 유지)
-    participant.forEach((name) => {
-        participantList[name] = (participantList[name] || 0) + 1
-    })
-    
-    // completion 순회하면서 participantList에 있으면 카운트 -1
-    completion.forEach((name) => {
-        if(participantList[name]) {
-               participantList[name] = participantList[name] - 1
+    participant.forEach(participant_el => {
+        if(participants[participant_el] > 0) {
+            participants[participant_el]++;    
+        } else {
+            participants[participant_el] = 1;    
         }
     })
     
-    // participantList에서 for...in 문 순회하면서 answer 반환
-    for(const person in participantList) {
-        if(participantList[person] > 0) {
-            answer = person
+    completion.forEach(completion_el => {
+        if(participants[completion_el] > 0) {
+            participants[completion_el]--; 
+        }
+    })
+    
+    for(const participantPerson in participants) {
+        if(participants[participantPerson] > 0) {
+            answer = participantPerson;
         }
     }
-    
     
     return answer;
 }
