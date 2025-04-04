9d6ce8a048e95d4f882479b2780f09ec774a5eca
yunsungLEE <135799803+yunsunglee2@users.noreply.github.com>
2025-04-04 20:04:08 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 5.61 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
@@ -1,42 +1,48 @@
 function solution(n, m) {
-    var answer = [];
-    const n_최대공약수_리스트 = [];
-    const m_최대공약수_리스트 = [];
+    const answer = [];
+    const nArray = [];
+    const mArray = [];
+    let firstEl = 0;
+    let lastEl = 0;
     
-    // 배열 맨앞에 최대공약수, 그 다음에 최소공배수 
-    
-    // ex) 12, 4
-    // 약수 구하기
-    // 12 - 1,2,3,4,6,12
-    // 4  - 1,2,4
-    
-    // 두 개의 약수 리스트 중에서 가장 큰 약수 => 최대 공약수
     for(let i = 1; i <= n; i++) {
-        const 나머지 = n%i;
-        if(나머지 === 0) {
-            n_최대공약수_리스트.push(i);
-        }
+        if(n%i === 0) {
+            nArray.push(i)
+        }    
     }
     
-        for(let j = 1; j <= m; j++) {
-        const 나머지 = m%j;
-        if(나머지 === 0) {
-            m_최대공약수_리스트.push(j);
-        }
+    for(let j = 1; j <= m; j++) {
+        if(m%j === 0) {
+            mArray.push(j)
+        }    
     }
-    console.log(n_최대공약수_리스트)
-    console.log(m_최대공약수_리스트)
     
-    // 공통 약수 리스트에서 가장 큰수 
-    const 공통약수 = n_최대공약수_리스트.filter(value => m_최대공약수_리스트.includes(value))
+    console.log(nArray, '--nArray--');
+    console.log(mArray, '--mArray--');
     
-    const 최대공약수 = Math.max(...공통약수);
-
-
-    // 최소공배수는 두수의 곱을 최대공약수로 나눈 값  
-    const 최소공배수 = n*m/최대공약수;
+    // 최대 공약수 구하기
+    const commonArray = [];
+    mArray.forEach(el => {
+        nArray.forEach(el_2 => {
+            if(el === el_2) {
+                    commonArray.push(el_2)    
+            }
+        })
+    })
     
-    answer.push(최대공약수, 최소공배수)
+    console.log(commonArray, '--commonArray--');
+    
+    if(nArray.length === 2 && mArray.length === 2) {
+        firstEl = 1;
+    } else {
+        firstEl = Math.max(...commonArray);
+    }
+    
+    // 최소 공배수 구하기
+    lastEl = n*m/firstEl;
+    
+    answer[0] = firstEl;
+    answer[1] = lastEl;
     
     return answer;
 }
