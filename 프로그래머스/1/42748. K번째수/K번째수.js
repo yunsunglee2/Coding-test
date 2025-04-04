1eac0e1d17641f865c774e65800830aa21f9f870
yunsungLEE <135799803+yunsunglee2@users.noreply.github.com>
2025-04-04 17:37:09 +0900

    [level 1] Title: K번째수, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,20 +1,17 @@
 function solution(array, commands) {
     var answer = [];
-    // commands를 순회하면서 i~j까지의 배열을 구한다. 
-    
-    // k 번째 수를 answer에 push 한다.
-    
-    commands.forEach((command) => {
+    // commands를 순회하면서 array를 자르고 정렬
+    commands.forEach(command => {
         const i = command[0];
         const j = command[1];
         const k = command[2];
-        // i,j 번째 숫자를 모두 포함한다.
-        // slice는 첫 번째 인자 포함, 두 번째 인자 미포함이다.
-        // [0,0,0,0] 1~3
+        
         const slicedArray = array.slice(i-1,j);
         const sortedArray = slicedArray.sort((a,b) => a-b);
+        
         answer.push(sortedArray[k-1]);
     })
     
+    // 반환한 배열을 k번째 수를 answer에 push 
     return answer;
 }
--- 프로그래머스/1/42748. K번째수/README.md
+++ 프로그래머스/1/42748. K번째수/README.md
@@ -1,10 +1,10 @@
 # [level 1] K번째수 - 42748 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748?language=javascript) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.08 ms
+메모리: 33.4 MB, 시간: 0.12 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 03일 20:59:24
+2025년 04월 04일 17:37:06
 
 ### 문제 설명
 
