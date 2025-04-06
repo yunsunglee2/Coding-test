c453358df97c8b154a53180b72af0c1c6c39077e
yunsungLEE <135799803+yunsunglee2@users.noreply.github.com>
2025-04-06 09:44:03 +0900

    [level 2] Title: 짝지어 제거하기, Time: 56.46 ms, Memory: 50.9 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -1,9 +1,15 @@
+// 순회하는 요소와 stack의 마지막 요소를 비교
+// 순회하는 요소와 stack을 마지막 요소가 같다면 pop을 실행 -> 실행하는 이유?
+// stack 배열에 순회했던 요소들을 집어 넣는다.
+// 순회했던 요소와 current 요소와 비교가 가능.
 function solution(s) {
     let answer = -1;
     const stack = [];
     
     for (let char of s) {
+        
         if(stack[stack.length-1] === char) {
+            
             stack.pop();
         } else {
             stack.push(char);
